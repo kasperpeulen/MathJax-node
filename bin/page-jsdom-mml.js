fb3e5902abab35ec0f97db5af62b5fb1295ae49a
Peter Krautzberger <p.krautzberger@gmail.com>
2016-05-27 14:01:26 +0200

    [drop mj-page] improve mj-page-like MML example

--- bin/page-jsdom-mml.js
+++ bin/page-jsdom-mml.js
@@ -4,11 +4,16 @@ mjAPI.start();
 var jsdom = require('jsdom').jsdom;
 var serializeDocument = require("jsdom").serializeDocument;
 
-var input = "<!DOCTYPE html><p> When <math> <mi>a</mi><mo>&#x2260;</mo><mn>0</mn> </math>, there are two solutions to <math> <mi>a</mi><msup><mi>x</mi><mn>2</mn></msup> <mo>+</mo> <mi>b</mi><mi>x</mi> <mo>+</mo> <mi>c</mi> <mo>=</mo> <mn>0</mn> </math>";
+var input = "<!DOCTYPE html><p> When <math> <mi>a</mi><mo>&#x2260;</mo><mn>0</mn> </math>, there are two solutions to <math> <mi>a</mi><msup><mi>x</mi><mn>2</mn></msup> <mo>+</mo> <mi>b</mi><mi>x</mi> <mo>+</mo> <mi>c</mi> <mo>=</mo> <mn>0</mn> </math></p>";
 
 var document = jsdom(input);
 var window = document.defaultView;
 var array = window.document.getElementsByTagName("math");
+
+var state = {};  // shared state among typeset requests (SVG <defs> for global sprite are stored here);  cf useGlobalCache
+var globalSVG = document.createElement("svg"); // the SVG sprite
+globalSVG.setAttribute("style","display:none;");
+document.body.appendChild(globalSVG);
 
 for (var i = 0; i < array.length; i++) {
     let index = i;
@@ -17,6 +22,8 @@ for (var i = 0; i < array.length; i++) {
       math: element.outerHTML,
       format: "MathML",
       svg: true,
+      useGlobalCache: true,
+      state: state,
       speakText: true
     }, function(data) {
       if (data.errors){
@@ -28,6 +35,7 @@ for (var i = 0; i < array.length; i++) {
         element.parentNode.replaceChild(svg, element);
       }
       if ( (index+1) === array.length) {
+        globalSVG.appendChild(document.importNode(state.defs,true));
         console.log(serializeDocument(document));
       }
     });
